135c18cd1220b9024b41e3078e4c5f3cb17d6399
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-29 21:49:02 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -19,7 +19,7 @@ function createToyCard(data){
   const btn = document.createElement("button");
   btn.className="like-btn";
   btn.innerHTML="Like <3";
-  btn.onClick = likes ;
+  btn.addEventListener("click",console.log(data.id)) ;
 
   card.append(header,img,para,btn);
   document.querySelector("#toy-collection").appendChild(card);
