4f67bfa1aee947a736df195fc30df32711415fa4
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-01-29 20:14:05 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,28 @@
 let addToy = false
+function getToys(){
+  fetch("http://localhost:3000/toys")
+  .then(resp => resp.json())
+  .then(data => createToyCard(data))
+  .catch(err => console.log(err));
+}
+function createToyCard(data){
+  const card = document.createElement("div");
+  card.class = "card";
+  const header = document.createElement("h2");
+  header.innerHTML="nader";
+  const img = document.createElement("img");
+  img.class="toy-avatar";
+  img.src="nader";
+  img.alt="toy pic";
+  const para = document.createElement("p");
+  para.innerHTML="nader";
+  const btn = document.createElement("button");
+  btn.class="like-btn";
+  btn.innerHTML="Like <3";
+
+  card.append(header,img,para,btn);
+  document.body.appendChild(card);
+}
 
 document.addEventListener("DOMContentLoaded", ()=>{
   const addBtn = document.querySelector('#new-toy-btn')
@@ -14,3 +38,23 @@ document.addEventListener("DOMContentLoaded", ()=>{
   })
 
 })
+
+function submitData(name,email){
+let configObj = {
+  method: "POST",
+  headers: {
+    "Content-Type": "application/json",
+    "Accept": "application/json"
+  },
+  body: JSON.stringify({
+    name ,
+    email
+  })
+};
+
+return fetch("http://localhost:3000/users", configObj)
+.then(resp => resp.json())
+.then(data => document.querySelector('span').textContent = data.id)
+.catch(err => document.querySelector('span').textContent = err );
+}
+
